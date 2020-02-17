72fa521a5b30397f7b76e562d8b19c662b1d5718
MattFauth <41974587+MattFauth@users.noreply.github.com>
2020-02-17 15:55:06 -0300

    Nova Funcao pro Banco de dados

--- funcoesBase.js
+++ funcoesBase.js
@@ -12,10 +12,26 @@
     document.calcform.display.value = ''
      delete lista;
   }
+
   function addInfo(){
     alert("fsfsdf")
     var textoDaInput = document.getElementById('tarefa');
     var arr = str.split("em");
     lista.push({arr[0],arr[1],false});
     document.getElementById('tarefa').value='222'
+  }
+
+  function salvaTrecos(){
+    let tarefa = document.getElementById("input").value //Aqui ele le o documento
+    segura = tarefa.split("em") //Faz o Split da string
+
+    novaTarefa = new tarefa(segura[1], segura[0], "false") //Cria o objeto
+
+    const stringUser = JSON.stringify(novaTarefa); //Usando a constante usa a funcao de string do JSON
+
+    localStorage.setItem('tarefa', stringUser); //Seta o item no localStorage
+    pega = localStorage.getItem('tarefa'); //Pega o item do localStorage
+    passa = JSON.parse(pega); //Passa o item do localStorage
+
+    document.getElementById('tarefa').value = "" //Reseta o valor do campo input pra ""
   }
