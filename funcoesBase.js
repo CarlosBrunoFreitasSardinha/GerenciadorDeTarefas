646ff37d109f82b06efdf5f2e1661adb2b9b91af
MattFauth <41974587+MattFauth@users.noreply.github.com>
2020-02-19 21:55:17 -0300

    SALVANDO NO LOCALSTORAGE
    AAAEEEE

--- funcoesBase.js
+++ funcoesBase.js
@@ -1,3 +1,23 @@
+  class Tarefa{
+    constructor(data, informacao, situacao){
+      this.data = data;
+      this.informacao = informacao;
+      this.situacao = situacao;
+    }
+
+    getInfo(){
+      return this.informacao;
+    }
+
+    getData(){
+      return this.data;
+    }
+
+    getSituacao(){
+      return this.situacao;
+    }
+  }
+
   function add() {
     var val = document.querySelector('#newItem').value;
     if((val !== undefined) && (val !== "")) {
@@ -14,24 +34,44 @@
   }
 
   function addInfo(){
-    alert("fsfsdf")
-    var textoDaInput = document.getElementById('tarefa');
-    var arr = str.split("em");
-    lista.push({arr[0],arr[1],false});
-    document.getElementById('tarefa').value='222'
+    a = localStorage.length;
+    i = 0;
+    while (i < localStorage.length){
+      i = i+1;}
+
+    alert("Adicionado")
+
+    let tarefa = document.getElementById('input').value;
+    arr = tarefa.split("em");
+
+
+    tarefa = new Tarefa(arr[1], arr[0], false);
+
+
+    const string = JSON.stringify(tarefa);
+
+    localStorage.setItem('tarefa' + i, string)
+
+    busca = localStorage.getItem('tarefa')
+    passa = JSON.parse(busca);
+
+    document.getElementById('input').value=""
   }
 
-  function salvaTrecos(){
-    let tarefa = document.getElementById("input").value //Aqui ele le o documento
-    segura = tarefa.split("em") //Faz o Split da string
+  function adicionarLinhaTabela(){
+    onload.call()
+    busca = localStorage.getItem('tarefa')
+    passa = JSON.parse(busca);
 
-    novaTarefa = new tarefa(segura[1], segura[0], "false") //Cria o objeto
-
-    const stringUser = JSON.stringify(novaTarefa); //Usando a constante usa a funcao de string do JSON
-
-    localStorage.setItem('tarefa', stringUser); //Seta o item no localStorage
-    pega = localStorage.getItem('tarefa'); //Pega o item do localStorage
-    passa = JSON.parse(pega); //Passa o item do localStorage
-
-    document.getElementById('tarefa').value = "" //Reseta o valor do campo input pra ""
+    var tabela = document.getElementById('tabela');
+		var numeroLinhas = tabela.rows.length;
+		var linha = tabela.insertRow();
+		var celula1 = linha.insertCell(0);
+		var celula2 = linha.insertCell(1);
+		var celula3 = linha.insertCell(2);
+		var celula4 = linha.insertCell(3);
+		celula1.innerHTML = passa.Tarefa[1];
+  	celula2.innerHTML = passa.Tarefa[0];
+  	celula3.innerHTML = "<button class='button-td-confirm'>V</button>";
+  	celula4.innerHTML = "<button class='button-td-cancel'>X</button>";
   }
