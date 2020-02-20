b04e81bcb56292053dbe1e4c67c178d3fcef57c3
Carlos Bruno F Sardinha <35373566+carlosifto@users.noreply.github.com>
2020-02-20 20:48:35 -0300

    Add files via upload
    mjolnir

--- funcoesBase.js
+++ funcoesBase.js
@@ -39,17 +39,17 @@
 
     inserirLinha(localStorage.length-1);
     document.getElementById('input').value="";
-    alert('Tarefa adicionada');
 }
   //função para inserir dados do localStorage na tabela
   function inserirLinha(i){
     busca = localStorage.getItem('tarefa'+i)
     passa = JSON.parse(busca);
 		var linha = document.getElementById('tabela').insertRow();
+
 		linha.insertCell(0).innerHTML = passa["data"];
   	linha.insertCell(1).innerHTML = passa["informacao"];
-  	linha.insertCell(2).innerHTML = "<button class='button-td-confirm' onclick='taxarLinha(this)'>V</button>";
-  	linha.insertCell(3).innerHTML = "<button class='button-td-cancel' onclick='redLinha(this)'>X</button>";
+  	linha.insertCell(2).innerHTML = "<button class=\"button-td-confirm\" id=\"tarefa"+i+"\" onclick=\"taxarLinha(this, 'tarefa"+i+")\">V</button>";
+  	linha.insertCell(3).innerHTML = "<button class=\"button-td-cancel\" id=\"tarefa"+i+"\" onclick=\"redLinha(this, 'tarefa"+i+"')\">X</button>";
     // if (passa["situacao"]) taxarLinha(linha);
     // else if(passa["situacao"]==false) taxarLinha(linha);
   }
@@ -61,14 +61,14 @@
 		}
 	}
   // funcao seta padrão concluído
-  function taxarLinha(linha) {
+  function taxarLinha(linha, ident) {
     pai = linha.parentNode.parentNode;
     pai.style.textDecoration = 'line-through';
     pai.style.background = 'lightgreen';
+    var itens = document.getElementById(ident).disabled = true;
   }
   // funcao seta padrão não concluído
-  function redLinha(linha) {
-    pai = linha.parentNode.parentNode;
-    pai.style.background = 'red';
-    pai.style.textDecoration = 'none';
+  function redLinha(linha, ident) {
+      localStorage.removeItem(ident);
+      linha.parentNode.parentNode.remove()
   }
